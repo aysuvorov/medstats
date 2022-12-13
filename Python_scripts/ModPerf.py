a9b86fc53dd5027aadd4116a5aaed107e7d68046
Lexxani <yourmedstat@gmail.com>
2022-12-13 13:06:49 +0300

    main

--- Python_scripts/ModPerf.py
+++ Python_scripts/ModPerf.py
@@ -23,7 +23,53 @@ warnings.filterwarnings("ignore")
 # Evaluation of a binary classification or a quantitative predictor 
 # in a binary classification
 
-def ModPerf_Binary(real, pred, num_resamples = 1000):
+def ModPerf_AUC(real, pred, num_resamples = 1000, ci95 = True):
+
+    if type(real) != np.ndarray:
+        real = np.array(real)
+
+    if type(pred) != np.ndarray:
+        pred = np.array(pred)
+
+    Y = np.array([real, pred]).T
+    Y = Y[~np.any(np.isnan(Y), axis=1)]
+
+    AUC = metrics.roc_auc_score(Y[:,0], Y[:,1])
+
+    if AUC < 0.5:
+        print('Inverted real class!!!')
+        
+        Y[:,0] = np.abs(Y[:,0] - 1)
+        AUC = metrics.roc_auc_score(Y[:,0], Y[:,1])
+
+    if ci95:
+
+        # Bootsrap CI-s
+        random.seed(0)
+        vals = []
+        for i in range(num_resamples):
+            a = np.array(random.choices(Y, k=len(Y)))
+            try:
+                vals = vals + [metrics.roc_auc_score(
+                    a[:,0], 
+                    a[:,1]
+                )]
+            except:
+                pass
+
+        vals = np.array(vals)
+
+        auc_ci = np.percentile(vals, [2.5, 97.5])
+
+        return[np.round(x, 3) for x in [AUC, auc_ci]]
+
+    else:
+
+        return np.round(AUC, 3)
+
+
+
+def ModPerf_Binary(real, pred, num_resamples = 1000, data_frame = True, ci95 = True):
 
     if type(real) != np.ndarray:
         real = np.array(real)
@@ -55,33 +101,49 @@ def ModPerf_Binary(real, pred, num_resamples = 1000):
     tn, fp, fn, tp = metrics.confusion_matrix(Y[:,0], Y[:,1]).ravel()
     auc, sens, spec, npv, ppv = _quality_point_est(Y[:,0], Y[:,1])
 
-    # Bootsrap CI-s
-    random.seed(0)
-    vals = []
-    for i in range(num_resamples):
-        a = np.array(random.choices(Y, k=len(Y)))
-        try:
-            vals = vals + [list(_quality_point_est(
-                a[:,0], 
-                a[:,1]
-            ))]
-        except:
-            pass
+    if ci95:
 
-    vals = np.array(vals)
+        # Bootsrap CI-s
+        random.seed(0)
+        vals = []
+        for i in range(num_resamples):
+            a = np.array(random.choices(Y, k=len(Y)))
+            try:
+                vals = vals + [list(_quality_point_est(
+                    a[:,0], 
+                    a[:,1]
+                ))]
+            except:
+                pass
 
-    auc_ci, sens_ci, spec_ci, npv_ci, ppv_ci = \
-        [np.percentile(vals[:,i][~np.isnan(vals)[:,i]], [2.5, 97.5]) for i in range(5)]
+        vals = np.array(vals)
 
-    # Making final data frame
-    names = ['tn', 'fp', 'fn', 'tp', 'auc', 'sens', 'spec', 'npv', 'ppv']
-    pe = [tn, fp, fn, tp, auc, sens, spec, npv, ppv]
-    ci = ['-', '-', '-', '-', auc_ci, sens_ci, spec_ci, npv_ci, ppv_ci]
+        auc_ci, sens_ci, spec_ci, npv_ci, ppv_ci = \
+            [np.percentile(vals[:,i][~np.isnan(vals)[:,i]], [2.5, 97.5]) for i in range(5)]
 
-    table = pd.DataFrame({'Chars': names, 'Point_est': pe, '95%CI': ci})
+        auc, sens, spec, npv, ppv, auc_ci, sens_ci, spec_ci, npv_ci, ppv_ci = \
+            [np.round(x, 3) for x in [auc, sens, spec, npv, ppv, auc_ci, sens_ci, \
+                spec_ci, npv_ci, ppv_ci]]
 
-    return table
+        # Making final data frame
+        names = ['tn', 'fp', 'fn', 'tp', 'auc', 'sens', 'spec', 'npv', 'ppv']
+        pe = [tn, fp, fn, tp, auc, sens, spec, npv, ppv]
+        ci = ['-', '-', '-', '-', auc_ci, sens_ci, spec_ci, npv_ci, ppv_ci]
 
+        if data_frame:
+            table = pd.DataFrame({'Chars': names, 'Point_est': pe, '95%CI': ci})
+            return table
+        else:
+            return[tn, fp, fn, tp, auc, sens, spec, npv, ppv,\
+                auc_ci, sens_ci, spec_ci, npv_ci, ppv_ci]
+
+    else:
+
+        auc, sens, spec, npv, ppv = \
+            [np.round(x, 3) for x in [auc, sens, spec, npv, ppv]]
+
+        return[tn, fp, fn, tp, auc, sens, spec, npv, ppv]
+      
 
 def ModPerf_Multiclass(real, pred, num_resamples = 1000): 
 
@@ -267,3 +329,28 @@ def ROCPlotter_Multiclass(real, pred, n_classes, title=None, plot=True, save_nam
 
 # +-----------------------------------------------------------------------------
 
+def threshold_getter(real, pred):
+
+    if type(real) != np.ndarray:
+        real = np.array(real)
+
+    if type(pred) != np.ndarray:
+        pred = np.array(pred)
+
+    Y = np.array([real, pred]).T
+    Y = Y[~np.any(np.isnan(Y), axis=1)]
+
+    g = pd.DataFrame()
+
+    for score in sorted(set(Y[:,1])):
+        tab = pd.Series(ModPerf_Binary(Y[:,0], \
+            np.array((Y[:,1] >= score).astype(int)), ci95 = False))
+        g = pd.concat([g, tab], axis=1)
+
+    g.columns = sorted(set(Y[:,1]))
+    g = g.T.reset_index()
+    g.rename(columns={'index':'Thres'}, inplace=True)
+    g.columns = ['Thres','tn', 'fp', 'fn', 'tp', 'auc', 'sens', 'spec', \
+        'npv', 'ppv']
+    g = g.drop('auc',1)
+    return g
