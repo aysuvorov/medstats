09343e01fb608fb6b15df4915b40ff2d89f1088d
Alexander Suvorov <yourmedstat@gmail.com>
2024-01-12 08:19:14 +0300

    111

--- R_scripts/_experimental/simulations.R
+++ R_scripts/_experimental/simulations.R
@@ -1,46 +1,28 @@
-https://cran.r-project.org/web/views/MixedModels.html
+# https://cran.r-project.org/web/views/MixedModels.html
 
-Power analysis and simulation
+# Power analysis and simulation
 
-Bootstrapping
+# Bootstrapping
 
-simr
-
-
-ContNoiser = \(x, method = 'unif', z_scores = 1, seed = 0) {
-    if (method == 'unif') {
-        set.seed = seed
-        noise = runif(
-            length(x),
-            min = 0,
-            # max = DescTools::MeanSE(x)
-            max = z_scores * sd(x, na.rm = TRUE)
-            )
-    } else if (method == 'norm') {
-        set.seed = seed
-        noise = rnorm(
-            length(x),
-            0,
-            # DescTools::MeanSE(x)
-            max = z_scores * sd(x, na.rm = TRUE)
-            )
-    } else {
-        print('error')
-    }
-    return(x + noise)
-}
+# simr
 
 # +-----------------------------------------------------------------------------
-# faux: Simulation for Factorial Designs
 library(faux)
+library(survival)
+library(survminer)
+library(gtsummary)
 
+# +-----------------------------------------------------------------------------
+# New functions
+
+
+
+# +-----------------------------------------------------------------------------
 source("https://raw.githubusercontent.com/aysuvorov/medstats/master/R_scripts/medstats.R")
 
 
 data(mtcars)
 mtcars = FactorTransformer(mtcars, 4)
-
-
 
 set.seed(0)
 faux_df = faux::sim_df(mtcars, 500)
@@ -49,18 +31,61 @@ mtcars |> names()
 
 rsmpld_df = data.frame()
 set.seed(0)
-for (i in rownames(mtcars) |> sample(1000, replace = TRUE)) {
+for (i in rownames(mtcars) |> sample(500, replace = TRUE)) {
     rsmpld_df = rbind(rsmpld_df, mtcars[i, ])
 }
-rsmpld_df = rsmpld_df |> mutate_if(is.numeric, ContNoiser(., method = 'norm'))
+rsmpld_df = rsmpld_df |> mutate_if(is.numeric, ContNoiser, z_scores = .5)
 
-lm(mpg ~ hp, mtcars) |> summary()
-lm(mpg ~ hp, faux_df) |> summary()
-lm(mpg ~ hp, rsmpld_df) |> summary()
+lm(mpg ~ hp + disp + hp + wt, mtcars) |> summary()
+lm(mpg ~ hp + disp + hp + wt, faux_df) |> summary()
+lm(mpg ~ hp + disp + hp + wt, rsmpld_df) |> summary()
 
 plot(mpg ~ hp, rsmpld_df)
+plot(mpg ~ hp, mtcars)
+
 
 mtcars |> mutate_all(as.numeric) |> summarise_all(sd)
 rsmpld_df |> mutate_all(as.numeric) |> summarise_all(sd)
 
-ContNoiser(seq(18), z_scores = 100)
+# Survival
+data(cancer, package="survival")
+cancer <- 
+  cancer %>% 
+  mutate(
+    status = recode(status, `1` = 0, `2` = 1)
+  )
+
+lung = data.frame()
+set.seed(0)
+for (i in rownames(cancer) |> sample(100, replace = TRUE)) {
+    lung = rbind(lung, cancer[i, ])
+}
+
+rsmpld_df = data.frame()
+set.seed(0)
+for (i in rownames(lung) |> sample(300, replace = TRUE)) {
+    rsmpld_df = rbind(rsmpld_df, lung[i, ])
+}
+rsmpld_df = rsmpld_df |> mutate_at(vars(inst, age, time, ph.karno, pat.karno, meal.cal, wt.loss), ContNoiser, 
+    method = 'unif', z_scores = 1)
+
+ggsurvplot(survfit(Surv(time, status) ~ sex, data = lung), data = lung, ggtheme = theme_bw(), 
+    conf.int = TRUE, pval = TRUE)
+
+ggsurvplot(survfit(Surv(time, status) ~ sex, data = rsmpld_df), data = rsmpld_df, ggtheme = theme_bw(), 
+    conf.int = TRUE, pval = TRUE)
+
+coxph(Surv(time, status) ~ sex + age + wt.loss + ph.ecog, data = lung) |> 
+  tbl_regression(exp = TRUE) |> as_tibble()
+coxph(Surv(time, status) ~ sex + age + wt.loss + ph.ecog, data = rsmpld_df) |> 
+  tbl_regression(exp = TRUE) |> as_tibble()
+
+lung |>
+    ggplot(aes(age, wt.loss, col = factor(status))) +
+    geom_point() +
+    geom_smooth()
+
+rsmpld_df |>
+    ggplot(aes(age, wt.loss, col = factor(status))) +
+    geom_point() +
+    geom_smooth()
