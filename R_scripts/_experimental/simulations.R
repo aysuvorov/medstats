dc992592e072e8d77eeaf4d9ad4f1c3346f30a58
Alexander Suvorov <yourmedstat@gmail.com>
2024-01-15 13:07:12 +0300

    111

--- R_scripts/_experimental/simulations.R
+++ R_scripts/_experimental/simulations.R
@@ -89,3 +89,63 @@ rsmpld_df |>
     ggplot(aes(age, wt.loss, col = factor(status))) +
     geom_point() +
     geom_smooth()
+
+
+# +-----------------------------------------------------------------------------
+# Boot library
+
+library(boot)
+library(DescTools)
+
+set.seed(0)
+vec1 = seq(80)
+vec2 = vec1 + rnorm(length(vec1))
+
+MeanDiffCI(vec1, vec2)
+
+df = data.frame(cbind(vec1, vec2))
+
+mean.diff <- function(dataframe, indexVector) {
+  return(mean(dataframe[indexVector,1] - dataframe[indexVector,2]))
+}
+
+mean(df[,1]) - mean(df[,2])
+
+t.test(vec1, vec2)
+
+(bts = boot(df, mean.diff, R = 1000) |> 
+  boot.ci(conf = 0.95))
+
+bts |> hist()
+str(bts)
+
+library(simpleboot)
+
+b <- two.boot(vec1, vec2, mean, R = 1000)
+boot.ci(b)  ## No studentized confidence intervals
+hist(b)
+
+M = cbind(vec1, vec2)
+
+mean.diff <- function(mtx, indexVector) {
+  return(mean(mtx[indexVector,1] - mtx[indexVector,2]))
+}
+
+mean.diff(M)
+
+(bts = boot(M, mean.diff, R = 1000) |> 
+  boot.ci(conf = 0.95))
+
+0.0398973 + 2*0.1011916
+
+ind = c(rep(1, length(vec1)), rep(2, length(vec2)))
+as.numeric(table(ind))
+
+mean.diff <- function(D, d) {
+  E=D[d,]
+  return(mean(E$vec1[d]) - mean(E$vec2))
+}
+
+boot(vec1, mean, R = 1000) |> boot.ci(type="basic")
+
+two.boot(vec1, vec2, mean, R = 1000)
